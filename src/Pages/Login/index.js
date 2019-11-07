0dbe9a9b3203eb2b45ca80be46e767879151c4b1
DanPendragom <carlos.coutinho@etec.sp.gov.br>
2019-11-07 18:41:53 -0300

    Adicionando dados

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,15 +1,53 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
-export default class Login extends Component{
-    render(){
-        return(
+export default class Login extends Component {
+    
+    state = {
+        login: '',
+        senha: ''
+    }
+
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const {
+            login,
+            senha,
+        } = this.state;
+
+        const logar = {
+            login: login,
+            senha: senha
+        }
+
+        console.log(logar);
+    }
+
+    render() {
+        return (
             <div className="container">
-                <form>
+                <form onSubmit={this.handleSubmit} >
                     <h2>Bem vindo Usuario</h2>
-                    <input placeholder="Email" type="email"/>
-                    <input placeholder="Senha" type="password"/>
-                    <button>ENTRAR</button>
+                    <input 
+                        onChange={this.handleInputChange} 
+                        value={this.state.login}
+                        name="login"
+                        placeholder="Email"  
+                    />
+
+                    <input onChange={this.handleInputChange} 
+                        value={this.state.senha} 
+                        placeholder="Senha" 
+                        name="senha"
+                        type="password" 
+                    />
+
+                    <button type="submit">ENTRAR</button>
                 </form>
             </div>
         );
